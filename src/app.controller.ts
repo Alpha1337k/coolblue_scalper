3f26e08566cb4e76406864765bed2230ff85e19d
Alpha1337k <ozziekruithof@gmail.com>
2021-09-07 12:22:15 +0200

    Feat: logo && Chore: tabs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,15 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get("items/:id")
-  async getItems(@Param() params): Promise<string> {
-	  return await this.appService.createItemList(params.id as string);
-  }
+	@Get("items/:id")
+	async getItems(@Param() params): Promise<string> {
+		return await this.appService.createItemList(params.id as string);
+	}
 
-  @Get("catagories")
-  async getCatagories(): Promise<string> {
-	  return await this.appService.getCatagories();
-  }
+	@Get("catagories")
+	async getCatagories(): Promise<string> {
+		return await this.appService.getCatagories();
+	}
 }
